15e30f2bdf33f6c3e9cdb079721fbf57b7319fbf
mv740 <michalwozniak@live.ca>
2015-03-29 19:52:20 -0400

    webrequesthandler : if user not found show error page

--- src/network/handler/WebRequestHandler.java
+++ src/network/handler/WebRequestHandler.java
@@ -17,7 +17,7 @@ public class WebRequestHandler {
 
         //search query
         String httpmessage = "";
-//08a0ff
+
 
         httpmessage += "HTTP/1.1 200 Okay\r\nContent-Type: text/html; charset=ISO-8859-4 \r\n\r\n";
         httpmessage += "<!DOCTYPE html><html lang='en'> <head><title>Twibbler</title><link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>"
@@ -25,31 +25,45 @@ public class WebRequestHandler {
                 + "<div class='container'>"
                 +"<br/>"
                 + "<div class='text-center'><img src=\"http://s1.postimg.org/fljap34cv/Twibbler_Banner.jpg\" alt=\"Twibbler Logo\" ></div>"
-                +"<br/>"
-                + "<div><table class='table table-striped text-center'>";
-        httpmessage += "<tr>"
-                + "<td class='info' colspan='4'><h3 class='text-center'>" + username + " Twibbles feed" + "</h3></td>"
-                + "</tr>";
-        httpmessage += "<tr>"
-                + "<td><strong>Username</strong></td>"
-                + "<td><strong>Id</strong></td>"
-                + "<td><strong>Message</strong></td>"
-                + "<td><strong>Date</strong></td>"
-                + "</tr>";
+                +"<br/>";
 
-        System.out.println(username);
-        System.out.println(username.length());
+
+        //System.out.println(username);
+        //System.out.println(username.length());
         List<TwibblePost> posts = MySQLAccess.getTwibbler(username);
-        System.out.println("size :" + posts.size());
-        for (int i = 0; i < posts.size(); i++) {
+        //System.out.println("size :" + posts.size());
 
+        if(!posts.isEmpty())
+        {
+            httpmessage +="<div><table class='table table-striped text-center'>"
+                    +"<tr>"
+                    + "<td class='info' colspan='4'><h3 class='text-center'>" + username + " Twibbles feed" + "</h3></td>"
+                    + "</tr>";
             httpmessage += "<tr>"
-                    + "<td>" + posts.get(i).getUsername() + "</td>"
-                    + "<td>" + posts.get(i).getId() + "</td>"
-                    + "<td>" + posts.get(i).getContent() + "</td>"
-                    + "<td>" + posts.get(i).getDatetime() + "</td>"
+                    + "<td><strong>Username</strong></td>"
+                    + "<td><strong>Id</strong></td>"
+                    + "<td><strong>Message</strong></td>"
+                    + "<td><strong>Date</strong></td>"
+                    + "</tr>";
+
+            for (int i = 0; i < posts.size(); i++) {
+
+                httpmessage += "<tr>"
+                        + "<td>" + posts.get(i).getUsername() + "</td>"
+                        + "<td>" + posts.get(i).getId() + "</td>"
+                        + "<td>" + posts.get(i).getContent() + "</td>"
+                        + "<td>" + posts.get(i).getDatetime() + "</td>"
+                        + "</tr>";
+
+            }
+        }
+        else{
+            httpmessage +="<div><table class='table table-striped text-center'>"
+                    +"<tr>"
+                    + "<td class='danger' colspan='4'><h3 class='text-center'>ERROR: " + username + " NOT FOUND!  " + "</h3></td>"
                     + "</tr>";
         }
+
         //System.out.println("testuser=");
         httpmessage += "</table></div></div></body></html>";
 
